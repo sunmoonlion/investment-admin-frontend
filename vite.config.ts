b492fe74549848a4a511e1a53eafbd0da29b5e2b
sunmoon <13701819268@example.com>
2026-06-15 10:30:29 +0800

    chore: initialize investment-admin-frontend

--- vite.config.ts
+++ vite.config.ts
@@ -180,8 +180,8 @@ export default defineConfig(({ mode, command }) => {
           VitePWA({
             injectRegister: 'auto',
             manifest: {
-              name: 'Tpl Admin CSR',
-              short_name: 'Tpl Admin',
+              name: 'Investment Admin CSR',
+              short_name: 'Investment Admin',
               theme_color: '#ffffff',
               icons: [
                 {
